ae0795bfbfe5f7600a254e43ecdb362d5be484c5
Misaki Koonce <misaki.koonce@gmail.com>
2019-11-15 14:49:00 -0800

    Add gatsby-config to read markdown pages.

---
+++ gatsby-config.js
@@ -0,0 +1,9 @@
+plugins: [
+    {
+        resolve: `gatsby-source-filesystem`,
+        options: {
+            name: `markdown-pages`,
+            path: `${misakikoonce.com/src/markdown-pages}`,
+        },
+    },
+]
