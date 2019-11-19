aad4525e8d7d2de89e141e3a5402fbb758455593
Misaki Koonce <misaki.koonce@gmail.com>
2019-11-18 21:42:13 -0800

    Add markdown pages.

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,9 @@ plugins: [
         resolve: `gatsby-source-filesystem`,
         options: {
             name: `markdown-pages`,
-            path: `${misakikoonce.com/src/markdown-pages}`,
+            path: `${/Users/misakimichy/src/misakikoonce.com/src/markdown-pages}`,
+            name: `markdown-pages`,
         },
     },
+    `gatsby-transformer-remark`,
 ]
