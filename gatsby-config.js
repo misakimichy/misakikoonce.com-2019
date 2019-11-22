db4169d498d30910237bb599b3d612b8a606f628
Misaki Koonce <misaki.koonce@gmail.com>
2019-11-21 18:11:38 -0800

    Add plugins.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,29 @@
-plugins: [
-    {
-        resolve: `gatsby-source-filesystem`,
-        options: {
-            name: `markdown-pages`,
-            path: `${/Users/misakimichy/src/misakikoonce.com/src/markdown-pages}`,
-            name: `markdown-pages`,
+module.exports = {
+    siteMetadata: {
+        title: `Misaki Koonce`,
+        description: `Misaki Koonce portfolio by Gatsby`,
+        copyrights: 'Misaki Koonce',
+        author: `@misakimichy`,
+        logo: {
+          src: '',
+          alt: '',
         },
     },
-    `gatsby-transformer-remark`,
-]
+    plugins: [
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `markdown-pages`,
+                path: `${__dirname}/src/markdown-pages`,
+                name: `markdown-pages`,
+            },
+        },
+        `gatsby-transformer-remark`,
+        {
+            resolve: `gatsby-plugin-typography`,
+            options: {
+              pathToConfigModule: `src/utils/typography`,
+            },
+        },
+    ]
+}
